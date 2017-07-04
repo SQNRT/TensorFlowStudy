0e60a32c2721413f136386541d92e774729c2cd3
Alice <Alice@ai.cn>
2017-07-04 16:16:18 +0800

    Correct some error in 6-4.

--- 6-4resnet_v2.py
+++ 6-4resnet_v2.py
@@ -30,8 +30,8 @@ def stack_blocks_dense(net, blocks, outputs_collections = None):
             for i, unit in enumerate(block.args):
                 with tf.variable_scope('unit_%d' % (i + 1), values = [net]):
                     unit_depth, unit_depth_bottleneck, unit_stride = unit
-                    net = block.unit_fn(net, depth = unit_depth, unit_depth_bottleneck = unit_depth_bottleneck, stride = unit_stride)
-            net = slim.utils.collect_name_outputs(outputs_collections, sc.name, net)
+                    net = block.unit_fn(net, depth = unit_depth, depth_bottleneck = unit_depth_bottleneck, stride = unit_stride)
+            net = slim.utils.collect_named_outputs(outputs_collections, sc.name, net)
     
     
     return net
@@ -42,7 +42,7 @@ def resnet_arg_scope(is_training = True, weight_decay = 0.0001, batch_norm_decay
     batch_norm_params = {'is_training': is_training, 'decay': batch_norm_decay, 'epsilon': batch_norm_epsilon, 'scale': batch_norm_scale, 'updates_collections': tf.GraphKeys.UPDATE_OPS,}
 
 
-    with slim.arg_scope([slim.conv2d], weights_regularizer = slim.l2_regularizer(weight_decay)), weights_initializer = slim.variance_scaling_initializer(), activation_fn = tf.nn.relu, normalizer_fn = slim.batch_norm, normalizer_params = batch_norm_params):
+    with slim.arg_scope([slim.conv2d], weights_regularizer = slim.l2_regularizer(weight_decay), weights_initializer = slim.variance_scaling_initializer(), activation_fn = tf.nn.relu, normalizer_fn = slim.batch_norm, normalizer_params = batch_norm_params):
         with slim.arg_scope([slim.batch_norm], **batch_norm_params):
             with slim.arg_scope([slim.max_pool2d], padding = 'SAME') as arg_sc:
                 return arg_sc
@@ -59,7 +59,7 @@ def bottleneck(inputs, depth, depth_bottleneck, stride, outputs_collections = No
         
 
         residual = slim.conv2d(preact, depth_bottleneck, [1, 1], stride = 1, scope = 'conv1')
-        residual = conv2d_same(residual, depth_bottleneck, 3, stride, scope = scope = 'conv2')
+        residual = conv2d_same(residual, depth_bottleneck, 3, stride, scope = 'conv2')
         residual = slim.conv2d(residual, depth, [1, 1], stride = 1, normalizer_fn = None, activation_fn = None, scope = 'conv3')
 
 
@@ -91,19 +91,19 @@ def resnet_v2(inputs, blocks, num_classes = None, global_pool = True, include_ro
             return net, end_points
 
 def resnet_v2_50(inputs, num_classes = None, global_pool = True, reuse = None, scope = 'resnet_v2_50'):
-    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)], Block('block2', bottleneck, [(512, 128, 1)] * 3 + [(512, 128, 2)], Block('block3', bottleneck, [(1024, 256, 1)] * 5 + [(1024, 256, 2)], Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
+    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]), Block('block2', bottleneck, [(512, 128, 1)] * 3 + [(512, 128, 2)]), Block('block3', bottleneck, [(1024, 256, 1)] * 5 + [(1024, 256, 2)]), Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
     return resnet_v2(inputs, blocks, num_classes, global_pool, include_root_block = True, reuse = reuse, scope = scope)
 
 def resnet_v2_101(inputs, num_classes = None, global_pool = True, reuse = None, scope = 'resnet_v2_101'):
-    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)], Block('block2', bottleneck, [(512, 128, 1)] * 3 + [(512, 128, 2)], Block('block3', bottleneck, [(1024, 256, 1)] * 22 + [(1024, 256, 2)], Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
+    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]), Block('block2', bottleneck, [(512, 128, 1)] * 3 + [(512, 128, 2)]), Block('block3', bottleneck, [(1024, 256, 1)] * 22 + [(1024, 256, 2)]), Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
     return resnet_v2(inputs, blocks, num_classes, global_pool, include_root_block = True, reuse = reuse, scope = scope)
 
 def resnet_v2_152(inputs, num_classes = None, global_pool = True, reuse = None, scope = 'resnet_v2_152'):
-    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)], Block('block2', bottleneck, [(512, 128, 1)] * 7 + [(512, 128, 2)], Block('block3', bottleneck, [(1024, 256, 1)] * 35 + [(1024, 256, 2)], Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
+    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]), Block('block2', bottleneck, [(512, 128, 1)] * 7 + [(512, 128, 2)]), Block('block3', bottleneck, [(1024, 256, 1)] * 35 + [(1024, 256, 2)]), Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
     return resnet_v2(inputs, blocks, num_classes, global_pool, include_root_block = True, reuse = reuse, scope = scope)
 
 def resnet_v2_200(inputs, num_classes = None, global_pool = True, reuse = None, scope = 'resnet_v2_200'):
-    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)], Block('block2', bottleneck, [(512, 128, 1)] * 23 + [(512, 128, 2)], Block('block3', bottleneck, [(1024, 256, 1)] * 35 + [(1024, 256, 2)], Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
+    blocks = [Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]), Block('block2', bottleneck, [(512, 128, 1)] * 23 + [(512, 128, 2)]), Block('block3', bottleneck, [(1024, 256, 1)] * 35 + [(1024, 256, 2)]), Block('block4', bottleneck, [(2048, 512, 1)] * 3)]
     return resnet_v2(inputs, blocks, num_classes, global_pool, include_root_block = True, reuse = reuse, scope = scope)
 
 from datetime import datetime
